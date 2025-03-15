2a8752470f4c63cb582e9c9054ac3b97b186552f
N0G0Z0 <xavier-nogueira@hotmail.com>
2025-03-15 16:56:55 +0000

    db primeiro objeto

--- project/sgrhapp/models.py
+++ project/sgrhapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+""" # Create your models here.
 class Study(models.Model):
     class Type(models.TextChoices):
         EXTRACT = "extraction", "extraction"
@@ -22,5 +22,16 @@ class Study(models.Model):
         db_table = 'study'
 
     def __str__(self):
-        return self.name + ' by ' + self.user.username
+        return self.name + ' by ' + self.user.username """
     
+    # Create your models here.
+class Cliente(models.Model):
+    cliente_id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=512, blank=True, null=True)
+    morada = models.CharField(max_length=512, blank=True, null=True)
+    dt_criacao = models.DateTimeField(default=datetime.now())
+    email = models.CharField(max_length=512, blank=True, null=True)
+    telem = models.IntegerField(max_length=15, blank=True, null=True)
+    telef = models.IntegerField(max_length=15, blank=True, null=True)
+    class Meta:
+        db_table = 'cliente'
